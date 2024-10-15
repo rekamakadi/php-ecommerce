19f394a3c2e52008241ab53f7395dceeab580379
rekamakadi <r.makadi@gmail.com>
2024-10-15 23:09:47 +0200

    [feat] add content section - products

--- index.php
+++ index.php
@@ -62,10 +62,55 @@
         <p class="text-center">With the right tools possibilities are endless</p>
     </div>
 
+    <!-- content -->
+    <div class="row">
+        <div class="col-md-10">
+            <!-- products -->
+            <div class="row">
+                <div class="col-md-4 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-info">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-info">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View more</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mb-2">
+                    <div class="card" style="width: 18rem;">
+                        <img src="..." class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-info">Add to cart</a>
+                            <a href="#" class="btn btn-secondary">View more</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-2">
+            <!-- sidenav -->
+        </div>
+    </div>
+
     <!-- footer -->
-    <!-- <div class="bg-info p-3 text-center">
+    <div class="bg-info p-3 text-center">
         <p>All rights reserved * eCom * 2024</p>
-    </div> -->
+    </div>
 
     <!-- bootstrap js link-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
