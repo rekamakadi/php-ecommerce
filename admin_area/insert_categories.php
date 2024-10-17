28056aba30966fa089caff6d963131130e98e395
rekamakadi <r.makadi@gmail.com>
2024-10-17 14:27:57 +0200

    [feat] add insert_categories.php

---
+++ admin_area/insert_categories.php
@@ -0,0 +1,10 @@
+<form action="" method="post" class="mb-2">
+    <div class="input-group w-90 mb-2">
+        <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+        <input type="text" class="form-control" name="cat-title" placeholder="Insert Categories" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    <div class="input-group w-10 mb-2 m-auto">
+        <!-- <input type="submit" class="form-control bg-info" name="insert-cat" value="Insert Categories" aria-label="Username" aria-describedby="basic-addon1"> -->
+        <button type="submit" class="bg-info p-2 my-3 border-0">Insert Categories</button>
+    </div>
+</form>
