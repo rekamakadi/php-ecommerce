48337f31df3e6626d785ce281c038c1efdbca82c
rekamakadi <r.makadi@gmail.com>
2024-10-24 00:47:59 +0200

    [rcc] exclude getproducts, getbrands, getcategories functions

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include('includes/connect.php');
+include('functions/common_function.php');
 ?>
 
 <!DOCTYPE html>
@@ -70,31 +70,8 @@ include('includes/connect.php');
 
                 <!-- fetching products -->
                 <?php
-                    $select_query = "SELECT * FROM products order by rand()";
-                    $result_query = mysqli_query($con, $select_query);
-                    while ($row = mysqli_fetch_assoc($result_query)) {
-                        $product_id = $row['product_id'];
-                        $product_title = $row['product_title'];
-                        $product_description = $row['product_description'];
-                        $product_price = $row['product_price'];
-                        $category_id = $row['category_id'];
-                        $brand_id = $row['brand_id'];
-                        $product_image1 = $row['product_image1'];
-
-                        echo "<div class='col-md-4 mb-2'>
-                            <div class='card' style='width: 18rem;'>
-                                <img src='./admin_area/product_images/{$product_image1}' class='card-img-top' alt='...'>
-                                <div class='card-body'>
-                                    <h5 class='card-title'>{$product_title}</h5>
-                                    <p class='card-text'>{$product_description}</p>
-                                    <a href='#' class='btn btn-info'>Add to cart</a>
-                                    <a href='#' class='btn btn-secondary'>View more</a>
-                                </div>
-                            </div>
-                        </div>";
-                    }
+                getproducts();
                 ?>
-                
             </div>
         </div>
         <div class="col-md-2 bg-secondary p-0">
@@ -106,16 +83,7 @@ include('includes/connect.php');
                     </a>
                 </li>
                 <?php
-                $select_brands = "SELECT * FROM brands";
-                $result_brands = mysqli_query($con, $select_brands);
-
-                while ($row_data = mysqli_fetch_assoc($result_brands)) {
-                    $brand_title = $row_data['brand_title'];
-                    $brand_id = $row_data['brand_id'];
-                    echo "<li class='nav-item'>
-                    <a href='index.php?brand={$brand_id}' class='nav-link text-light'>{$brand_title}</a>
-                </li>";
-                }
+                getbrands();
                 ?>
             </ul>
             <ul class="navbar-nav me-auto text-center">
@@ -125,16 +93,7 @@ include('includes/connect.php');
                     </a>
                 </li>
                 <?php
-                $select_categories = "SELECT * FROM categories";
-                $result_categories = mysqli_query($con, $select_categories);
-
-                while ($row_data = mysqli_fetch_assoc($result_categories)) {
-                    $category_title = $row_data['category_title'];
-                    $category_id = $row_data['category_id'];
-                    echo "<li class='nav-item'>
-                    <a href='index.php?category={$category_id}' class='nav-link text-light'>{$category_title}</a>
-                </li>";
-                }
+                getcategories();
                 ?>
             </ul>
         </div>
