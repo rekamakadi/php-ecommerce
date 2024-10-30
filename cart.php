45958eac3a17edc3fef8d277effc32da1e53f69f
rekamakadi <r.makadi@gmail.com>
2024-10-30 11:51:44 +0100

    [feat] implement cart.php

--- cart.php
+++ cart.php
@@ -8,7 +8,7 @@ include('functions/common_function.php');
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ecommerce website (PHP + MySQL)</title>
+    <title>Ecommerce website - Cart Details</title>
     <!-- bootsstrap CSS link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- font awesome link -->
@@ -43,14 +43,7 @@ include('functions/common_function.php');
                         <li class="nav-item">
                             <a class="nav-link" href="cart.php"><i class="fa-solid fa-cart-shopping"></i><sup><?php cart_item() ?></sup></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Total Price: <?php total_cart_price() ?>/-</a>
-                        </li>
                     </ul>
-                    <form class="d-flex" action="search_product.php" method="get">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" name="search_data">
-                        <input type="submit" value="Search" class="btn btn-outline-light" name="search_data_product">
-                    </form>
                 </div>
             </div>
         </nav>
@@ -61,51 +54,47 @@ include('functions/common_function.php');
     cart();
     ?>
 
-
     <!-- header -->
     <div class="bg-light">
         <h3 class="text-center">eCom Store</h3>
         <p class="text-center">With the right tools possibilities are endless</p>
     </div>
 
-    <!-- content -->
-    <div class="row">
-        <div class="col-md-10">
-            <!-- products -->
-            <div class="row">
-
-                <!-- fetching products -->
-                <?php
-                get_all_products();
-                get_unique_categories();
-                get_unique_brands();
-                ?>
+    <!-- table -->
+    <div class="container">
+        <div class="row">
+            <table class="table table-bordered text-center">
+                <thead>
+                    <th>Product Title</th>
+                    <th>Product Image</th>
+                    <th>Quantity</th>
+                    <th>Total Price</th>
+                    <th>Remove</th>
+                    <th>Operations</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Title</td>
+                        <td>IMG</td>
+                        <td><input type="text" name="" id=""></td>
+                        <td>TP</td>
+                        <td><input type="checkbox"></td>
+                        <td>
+                            <p>Update</p>
+                            <p>Remove</p>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+            <!-- subtotal -->
+            <div class="d-flex mb-5">
+                <h4 class="px-3">Subtotal: <strong class="text-info">STP/-</strong></h4>
+                <a href="index.php"><button class="bg-info  p-3 border-0">Continue shopping</button></a>
+                <a href="#"><button class="bg-secondary  p-3 border-0 text-light">Checkout</button></a>
             </div>
         </div>
-        <div class="col-md-2 bg-secondary p-0">
-            <!-- sidenav -->
-            <ul class="navbar-nav me-auto text-center">
-                <li class="nav-item bg-info">
-                    <a href="#" class="nav-link text-light">
-                        <h4>Delivery Brands</h4>
-                    </a>
-                </li>
-                <?php
-                getbrands();
-                ?>
-            </ul>
-            <ul class="navbar-nav me-auto text-center">
-                <li class="nav-item bg-info">
-                    <a href="#" class="nav-link text-light">
-                        <h4>Categories</h4>
-                    </a>
-                </li>
-                <?php
-                getcategories();
-                ?>
-            </ul>
-        </div>
     </div>
+
 
     <!-- footer -->
     <?php include("./includes/footer.php") ?>
