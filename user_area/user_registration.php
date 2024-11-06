840fadcb0d6729d0934e54636d2ba73e50812f43
rekamakadi <r.makadi@gmail.com>
2024-11-07 00:54:14 +0100

    [feat] verify username + email + password

--- user_area/user_registration.php
+++ user_area/user_registration.php
@@ -72,13 +72,25 @@ if (isset($_POST['user_register'])) {
     $user_image_tmp = $_FILES['user_image']['tmp_name'];
     $user_ip = getIPAddress();
 
-    move_uploaded_file($user_image_tmp, "./user_images/$user_image");
-    $insert_query = "INSERT INTO user_table (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$user_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
-    $sql_execute = mysqli_query($con, $insert_query);
-    if ($sql_execute) {
-        echo "<script>alert('Data inserted successfully')</script>";
+
+    // select query
+    $select_query = "SELECT * FROM user_table WHERE username = '$user_username' or user_email = '$user_email'";
+    $result = mysqli_query($con, $select_query);
+    $rows_count = mysqli_num_rows($result);
+    if ($rows_count > 0) {
+        echo "<script>alert('Username or Email already exists')</script>";
+    } else if ($user_password != $confirm_user_password) {
+        echo "<script>alert('Passwords do not match')</script>";
     } else {
-        die(mysqli_error($con));
+        // insert query
+        move_uploaded_file($user_image_tmp, "./user_images/$user_image");
+        $insert_query = "INSERT INTO user_table (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$user_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
+        $sql_execute = mysqli_query($con, $insert_query);
+        if ($sql_execute) {
+            echo "<script>alert('Data inserted successfully')</script>";
+        } else {
+            die(mysqli_error($con));
+        }
     }
 }
 ?>
