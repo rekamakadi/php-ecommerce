aa37f0b871ec8d3a83d015129668c38c8f75df56
rekamakadi <r.makadi@gmail.com>
2024-11-07 01:02:50 +0100

    [feat] store hash_password

--- user_area/user_registration.php
+++ user_area/user_registration.php
@@ -65,6 +65,7 @@ if (isset($_POST['user_register'])) {
     $user_username = $_POST['user_username'];
     $user_email = $_POST['user_email'];
     $user_password = $_POST['user_password'];
+    $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
     $confirm_user_password = $_POST['confirm_user_password'];
     $user_address = $_POST['user_address'];
     $user_contact = $_POST['user_contact'];
@@ -84,7 +85,7 @@ if (isset($_POST['user_register'])) {
     } else {
         // insert query
         move_uploaded_file($user_image_tmp, "./user_images/$user_image");
-        $insert_query = "INSERT INTO user_table (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$user_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
+        $insert_query = "INSERT INTO user_table (username, user_email, user_password, user_image, user_ip, user_address, user_mobile) VALUES ('$user_username', '$user_email', '$hash_password', '$user_image', '$user_ip', '$user_address', '$user_contact')";
         $sql_execute = mysqli_query($con, $insert_query);
         if ($sql_execute) {
             echo "<script>alert('Data inserted successfully')</script>";
